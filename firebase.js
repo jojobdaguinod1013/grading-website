6730fa96761caeb87ec53e461af31a7ce877b03a
jojobdaguinod1013 <jojobdaguinod1013@gmail.com>
2026-08-25 15:14:11 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_APP.firebaseapp.com",
-  projectId: "YOUR_APP",
-  storageBucket: "YOUR_APP.appspot.com",
+  apiKey: "AIzaSyDCGScZAkoJa3sVKUhd56NFXxuxE8-4cME",
+  authDomain: "grading-website-101d1.firebaseapp.com",
+  projectId: "grading-website-101d1",
+  storageBucket: "grading-website-101d1.firebasestorage.app",
+  messagingSenderId: "984831495316",
+  appId: "1:984831495316:web:8b1bb081539e8f97e13881",
+  measurementId: "G-05EC78X903"
 };
 
 firebase.initializeApp(firebaseConfig);
