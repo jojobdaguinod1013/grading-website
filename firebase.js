b806a4f08e9caba5295e14033ccacb10e224c614
jojobdaguinod1013 <jojobdaguinod1013@gmail.com>
2026-08-25 15:31:29 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDCGScZAkoJa3sVKUhd56NFXxuxE8-4cME",
   authDomain: "grading-website-101d1.firebaseapp.com",
@@ -7,12 +8,3 @@ const firebaseConfig = {
   appId: "1:984831495316:web:8b1bb081539e8f97e13881",
   measurementId: "G-05EC78X903"
 };
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_APP.firebaseapp.com",
-  projectId: "YOUR_APP",
-  storageBucket: "YOUR_APP.appspot.com",
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
